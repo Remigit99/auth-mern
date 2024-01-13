870bb4515d3766ee0e5efee94951d9b078d1566d
Remigit99 <aderemibiodun1000@gmail.com>
2024-01-13 15:35:14 +0100

    Add content in the home page

--- client/src/component/Navbar.jsx
+++ client/src/component/Navbar.jsx
@@ -4,17 +4,17 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
     return (
-        <nav className="bg-red-600">
-            <div className="container nav__container">
-                <Link to="/home" >
-                    <h3>Remi</h3>
+        <nav className=" bg-slate-100 text-black py-4">
+            <div className="container nav__container flex justify-between align-middle ">
+                <Link to="/" >
+                    <h3 className="text-3xl">Remi</h3>
                 </Link>
 
 
-                <ul>
-                    <Link to="/about">About</Link>
-                    <Link to="/sign-in">Sign In</Link>
-                    <Link to="/sign-up">Sign Up</Link>
+                <ul className="flex justify-between gap-8 ">
+                    <Link to="/about" className="text-2xl">About</Link>
+                    <Link to="/sign-in" className="text-2xl">Sign In</Link>
+                    <Link to="/sign-up" className="text-2xl">Sign Up</Link>
                 </ul>
             </div>
         </nav>
