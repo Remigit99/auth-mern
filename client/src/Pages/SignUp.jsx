6e771794c062b58136c0cb2d57bf288bf4919e87
Remigit99 <aderemibiodun1000@gmail.com>
2024-01-15 10:08:57 +0100

    developed signup page

--- client/src/Pages/SignUp.jsx
+++ client/src/Pages/SignUp.jsx
@@ -1,15 +1,31 @@
 
-
+import { Link } from "react-router-dom"
 
 
 const SignUp = () => {
     return (
 
-        <main className="w-[600px] mx-auto">
+        <main className="w-[600px] mx-auto my-8 text-center">
             <h1 className="text-3xl mt-6">Sign Up</h1>
-            <p className="text-xl mt-6">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae minus id maxime? Impedit libero porro accusamus facere unde, amet deleniti consequuntur temporibus! Cupiditate saepe, modi laboriosam provident qui perspiciatis minus commodi fugiat. Pariatur dolorum necessitatibus quod labore, assumenda maxime quidem officiis et nam atque accusamus fuga ex quam repudiandae itaque laboriosam, praesentium accusantium iure delectus voluptates voluptatibus facere architecto sed quaerat. Consectetur necessitatibus nostrum quo quasi voluptas eaque ullam labore optio at excepturi, aliquam nesciunt harum odio quae aut praesentium, architecto fugit ipsum nemo deserunt esse nihil! Praesentium nihil quas numquam. Veritatis adipisci nobis vero et repellat alias temporibus perferendis?
-            </p>
+
+            <div className="sign__in my-12">
+                <form>
+                    <input type="text" name="username" id="username" placeholder="Username" autoComplete="off" className="bg-slate-100 w-full p-2 outline-none rounded-[0.5rem] mb-8" />
+
+                    <input type="email" name="email" id="email" placeholder="Email" autoComplete="off" className="bg-slate-100 w-full p-2 outline-none rounded-[0.5rem]" />
+
+                    <input type="password" name="password" id="password" placeholder="Password" autoComplete="off" className="bg-slate-100 w-full p-2 outline-none my-8 rounded-[0.5rem]" />
+
+                    <button className="flex justify-center align-center text-white bg-purple-800 w-full py-2 rounded-[0.5rem]">SIGN UP</button>
+
+                    <button className="text-center my-4 text-white bg-red-800 w-full py-2 rounded-[0.5rem] hover:opacity-0.5">CONTINUE WITH GOOGLE</button>
+
+                    <div>
+
+                        <p>Have an account? <button className=" text-purple-600"><Link to="/sign-up" />Sign In</button></p>
+                    </div>
+                </form>
+            </div>
         </main>
     )
 }
